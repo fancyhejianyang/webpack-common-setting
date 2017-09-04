2f1129a35a17595d83f59606eec03f97c86dfb97
fancyhejianyang <18651436384@163.com>
2017-09-04 23:10:03 +0800

    remove sass-cache

--- es6/main.js
+++ es6/main.js
@@ -1,5 +1,5 @@
+import "./css/main.css";
 import a from './a.js';
 import $ from '../jquery-wrap.js';
 var obj = window.document.body;
-obj.innerHTML = a()+1;
-console.log($("body").html()+1);
+obj.innerHTML = a();
